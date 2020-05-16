5c545932e94f37a2fa80fa29a210373a3eb65460
pratik-gh <pratik@particle41.com>
2020-05-16 18:35:42 +0530

    added add item and remove item functionality in checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -7,3 +7,22 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const clearItemFromCart = (cartItems, cartItemToClear) => {
+  cartItems = cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+  return cartItems;
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+
+  if (cartItemToRemove.quantity === 1) {
+    return clearItemFromCart(cartItems, cartItemToRemove);
+  }
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) => (cartItem.id === cartItemToRemove.id ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem));
+  }
+
+  // return [...cartItems, { ...cartItemToRemove, quantity: 1 }];
+};
